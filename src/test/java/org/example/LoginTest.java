ea2372b24df5ce2a07871f8dff9c9d8b7bc3c389
Anamitra-Biswas <abiswas1217@gmail.com>
2026-05-18 16:35:36 +0530

    Base TesT creation, Extended in Login test

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -1,25 +1,13 @@
 package org.example;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import net.bytebuddy.build.Plugin;
 import org.example.pages.InventoryPage;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 import org.example.pages.LoginPage;
+import org.testng.annotations.Test;
 
-public class LoginTest {
+public class LoginTest extends BaseTest{
 
-    WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+
 
     @Test(priority = 2)
     public void testSuccessfulTest(){
@@ -37,12 +25,5 @@ public class LoginTest {
         String title = driver.getTitle();
         System.out.println("Page title: " + title);
         assert title.equals("Swag Labs");
-    }
-
-
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
     }
 }
